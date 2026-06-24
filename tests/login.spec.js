046f7fbc8c46abba2093439ea688356d136448f0
Your Name <you@example.com>
2026-06-24 22:24:35 +0530

    Added title validation to login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,7 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('My First Test', async ({ page }) => {
   await page.goto('https://google.com');
+
+  await expect(page).toHaveTitle(/Google/);
 });
