0ce1a4b761ab8937c2cf8349f289db6f449d85cf
Liyaa Elizabeth Thomas <liyaaelizabeth01@gmail.com>
2026-06-25 09:23:02 +0530

    Version B

--- tests/signup.spec.js
+++ tests/signup.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('signup page practice test version B', async ({ page }) => {
+test('signup page version B', async ({ page }) => {
     await page.goto('https://practicetestautomation.com/practice-test-login/');
 
     await expect(page.locator('#username')).toBeVisible();
