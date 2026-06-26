25163462f764b54948e8c7f92c7c9da13fc79b4b
Elizabath Helen Felix <elizabathhelen08@gmail.com>
2026-06-26 11:52:00 +0530

    Updated pageTitle.spec.js

--- tests/pageTitle.spec.js
+++ tests/pageTitle.spec.js
@@ -4,6 +4,6 @@ test('Verify page title', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
 
-  await expect(page).toHaveTitle(/Swag Labs/);
+  await expect(page). toHaveTitle(/Swag Labs/);   
 
 });
